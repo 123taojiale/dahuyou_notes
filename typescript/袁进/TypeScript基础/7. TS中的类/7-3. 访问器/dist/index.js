0babd5f8d170051b0359fabf7feb5fe9fd59531a
dahuyou <2326433153@qq.com>
2022-06-15 16:46:48 +0800

    done 7-3

--- typescript/袁进/TypeScript基础/7. TS中的类/7-3. 访问器/dist/index.js
+++ typescript/袁进/TypeScript基础/7. TS中的类/7-3. 访问器/dist/index.js
@@ -1,16 +1,27 @@
 class User {
-    constructor(name = "dahuyou", age) {
+    constructor(name, _age) {
         this.name = name;
-        this.age = age;
+        this._age = _age;
         this.gender = "男";
-        this.publishNumber = 3;
-        this.curNumber = 0;
+        this._publishNumber = 3;
+        this._curNumber = 0;
         this.id = Math.random();
     }
+    set age(value) {
+        if (value < 0)
+            this._age = 0;
+        else if (value > 200)
+            this._age = 200;
+        else
+            this._age = value;
+    }
+    get age() {
+        return Math.floor(this._age);
+    }
     publish(title) {
-        if (this.curNumber < this.publishNumber) {
+        if (this._curNumber < this._publishNumber) {
             console.log(`${title}已发布`);
-            this.curNumber++;
+            this._curNumber++;
         }
         else {
             console.log("发布已达上线");
@@ -18,3 +29,7 @@ class User {
     }
 }
 const u = new User("dahuyou", 23);
+u.age = -1;
+u.age = 1.5;
+u.age = 10000;
+u.age;
