097fe34a3d955ff50d9af4e172671392ebb7eaab
dahuyou <2326433153@qq.com>
2022-06-17 07:46:51 +0800

    done 01

---
+++ vue/袁进/vue2/就业/01. 【vue】组件通信总结/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <HelloWorld @click.native="handleClick" ref="hello" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    handleClick() {
+      console.log(1);
+    },
+  },
+};
+</script>
---
+++ vue/袁进/vue2/就业/01. 【vue】组件通信总结/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div @click="handleClick">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  methods: {
+    handleClick() {
+      // console.log('helloworld is clicked');
+      this.$emit('click', 123);
+    }
+  }
+}
+</script>
