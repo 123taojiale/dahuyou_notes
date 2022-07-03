ccbfcde6d47b47f574e84f8874f89da0c5679bd3
dahuyou <dahuyou_top@163.com>
2022-07-03 09:51:34 +0800

    add test_002

---
+++ 0002 - vue2 进阶（袁进）/09. [vue]作用域插槽/test_002/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <ChildComp>
+    <!-- <template v-slot:slot1="{ data1 }">
+      <h1>父组件填 slot1 并获取到了子组件作用域中的数据：{{ data1 }}</h1>
+    </template> -->
+    <!-- 下面这种写法是上述写法的简写形式 -->
+    <template #slot1="{ data1 }">
+      <h1>父组件填 slot1 并获取到了子组件作用域中的数据：{{ data1 }}</h1>
+    </template>
+  </ChildComp>
+</template>
+
+<script>
+import ChildComp from "./components/ChildComp.vue";
+
+export default {
+  name: "App",
+  components: {
+    ChildComp,
+  },
+};
+</script>
---
+++ 0002 - vue2 进阶（袁进）/09. [vue]作用域插槽/test_002/src/components/ChildComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot name="slot1" :data1="data1">slot1 default value...</slot>
+    <slot name="slot2" :data2="data2">slot2 default value...</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      data1: 1,
+      data2: 2,
+    }
+  },
+}
+</script>
