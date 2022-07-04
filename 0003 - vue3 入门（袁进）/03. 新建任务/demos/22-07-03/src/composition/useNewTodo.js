e5d11551071a35aea0115c9841e0e93f4ab5beef
dahuyou <dahuyou_top@163.com>
2022-07-04 08:12:18 +0800

    完成 03. 新增任务联系

---
+++ 0003 - vue3 入门（袁进）/03. 新建任务/demos/22-07-03/src/composition/useNewTodo.js
@@ -0,0 +1,26 @@
+// 新增任务
+import {
+  ref
+} from "vue";
+import generateId from "../utils/generateId";
+
+export const newTodoRef = ref("");
+
+export function addNewTodo(todosRef) {
+  return () => {
+    // console.log(123);
+    // 去掉首尾空白，连续空白字符视作一个空格
+    let inputValue = newTodoRef.value.trim().replace(/\s+/g, " ");
+    console.log(inputValue);
+    // 校验是否为空
+    if (!inputValue) return;
+    // 创建任务对象
+    const todoObj = {
+      id: generateId(), // 任务 id
+      completed: false, // 任务是否完成
+      title: inputValue, // 任务的内容
+    }
+    todosRef.value.push(todoObj);
+    newTodoRef.value = "";
+  }
+}
