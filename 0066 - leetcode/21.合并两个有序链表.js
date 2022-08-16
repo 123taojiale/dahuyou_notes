eb5134afec9dfd8729be7a3a516c0299ed7bbc0d
dahuyou <dahuyou_top@163.com>
2022-08-17 07:48:44 +0800

    leetcode 21. merge two sorted lists

---
+++ 0066 - leetcode/21.合并两个有序链表.js
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=javascript
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode.cn/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (66.75%)
+ * Likes:    2610
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 1.7M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100
+ * l1 和 l2 均按 非递减顺序 排列
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * 22-08-17
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(list1, list2) {
+  if (!list1) return list2
+  if (!list2) return list1
+  let head, cur
+  if (list1.val >= list2.val) {
+    head = cur = list2
+    list2 = list2.next
+  } else {
+    head = cur = list1
+    list1 = list1.next
+  }
+  while(list1 && list2) {
+    if (list1.val >= list2.val) {
+      cur.next = list2
+      cur = cur.next
+      list2 = list2.next
+    } else {
+      cur.next = list1
+      cur = cur.next
+      list1 = list1.next
+    }
+  }
+  cur.next = list1 ? list1 : list2
+  return head
+};
+// @lc code=end
+
